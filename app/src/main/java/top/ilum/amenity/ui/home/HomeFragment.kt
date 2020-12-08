2756e1459c4ff85ad753649d7ecea89e1a0d8e64
Anastasia <ad4019809@gmail.com>
2020-12-08 23:01:53 +0300

    added map

--- app/src/main/java/top/ilum/amenity/ui/home/HomeFragment.kt
+++ app/src/main/java/top/ilum/amenity/ui/home/HomeFragment.kt
@@ -1,31 +1,93 @@
 package top.ilum.amenity.ui.home
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.CircleOptions
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PolygonOptions
+import kotlinx.android.synthetic.main.fragment_home.*
 import top.ilum.amenity.R
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), OnMapReadyCallback {
 
-    private lateinit var homeViewModel: HomeViewModel
+    lateinit var mapView: MapView
+    lateinit var googleMap: GoogleMap
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val v = inflater.inflate(R.layout.fragment_home, container, false)
+        mapView = v.findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.onResume()
+        try {
+            MapsInitializer.initialize(activity?.applicationContext)
+        } catch (e: Exception){
+            e.printStackTrace()
+        }
+        mapView.getMapAsync(this)
+        return v
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        btnCreatePolygon.text = "Выбрать фрагмент"
+        btnCreatePolygon.setOnClickListener {
+            btnCreatePolygon.text = "Выберите точки на карте"
+            createPolygon()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    private fun createPolygon() {
+        val positions = ArrayList<LatLng>()
+        googleMap.setOnMapClickListener { latLng -> positions += latLng
+            btnCreatePolygon.text = "Готово"
+            googleMap.addCircle(
+                CircleOptions().center(latLng).radius(1.0).strokeColor(Color.RED).fillColor(
+                    Color.RED))
+        }
+        btnCreatePolygon.setOnClickListener {
+            val polygonOptions = PolygonOptions()
+            positions.forEach {
+                polygonOptions.add(it)
+            }
+            polygonOptions.strokeColor(Color.RED).fillColor(Color.parseColor("#50FFB7B7"))
+            googleMap.addPolygon(polygonOptions)
+            btnCreatePolygon.text = "Выбрать фрагмент"
+        }
+    }
+
+    override fun onMapReady(p0: GoogleMap?) {
+        googleMap = p0 as GoogleMap
+    }
+
 }
