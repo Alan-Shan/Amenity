4e78e7a4dd2451bd1d793e84bf36d898426ef3bc
Anastasia <ad4019809@gmail.com>
2020-12-11 21:44:46 +0300

    adding markers

--- app/src/main/java/top/ilum/amenity/ui/home/HomeFragment.kt
+++ app/src/main/java/top/ilum/amenity/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package top.ilum.amenity.ui.home
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
 import android.graphics.Color
 import android.graphics.Point
 import android.os.Bundle
@@ -14,8 +15,8 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.MapsInitializer
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.PolygonOptions
+import com.google.android.gms.maps.model.*
+import com.google.maps.android.PolyUtil
 import kotlinx.android.synthetic.main.fragment_home.*
 import top.ilum.amenity.R
 import kotlin.math.roundToInt
@@ -30,7 +31,13 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
     private var destination = 1
 
     private var isMapMoveable = false
-    private var screenLeave = false
+    private var isMarker = false
+
+    private val items = arrayOf("Скамья", "Ограждение", "Фонарь", "Создать свой объект")
+    private var item = 0
+    private lateinit var polygon: Polygon
+
+//    private val mBottomSheetBehavior: BottomSheetBehavior<*>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -58,36 +65,86 @@ class HomeFragment : Fragment(), OnMapReadyCallback {
             btnCreatePolygon.visibility = View.GONE
             positions.removeAll(positions)
             googleMap.clear()
-        }
-        frame_layout.setOnTouchListener(View.OnTouchListener { _, motionEvent ->
-            if (isMapMoveable) {
-                val point = Point(motionEvent.x.roundToInt(), motionEvent.y.roundToInt())
-                val latLng = googleMap.projection.fromScreenLocation(point)
-                val latitude = latLng.latitude
-                val longitude = latLng.longitude
 
-                when (motionEvent.action) {
-                    MotionEvent.ACTION_DOWN -> {
-                        positions.add(LatLng(latitude, longitude))
-                        screenLeave = false
-                    }
-                    MotionEvent.ACTION_MOVE -> {
-                        positions.add(LatLng(latitude, longitude))
-                        screenLeave = false
-                    }
-                    MotionEvent.ACTION_UP -> {
-                        isMapMoveable = false
-                        source = 0
-                        destination = 1
-                        btnCreatePolygon.visibility = View.VISIBLE
-                        drawPolygon()
+            frame_layout.setOnTouchListener(View.OnTouchListener { _, motionEvent ->
+                if (isMapMoveable) {
+                    val point = Point(motionEvent.x.roundToInt(), motionEvent.y.roundToInt())
+                    val latLng = googleMap.projection.fromScreenLocation(point)
+                    val latitude = latLng.latitude
+                    val longitude = latLng.longitude
+
+                    when (motionEvent.action) {
+                        MotionEvent.ACTION_DOWN -> {
+                            positions.add(LatLng(latitude, longitude))
+                        }
+                        MotionEvent.ACTION_MOVE -> {
+                            positions.add(LatLng(latitude, longitude))
+                        }
+                        MotionEvent.ACTION_UP -> {
+                            isMapMoveable = false
+                            source = 0
+                            destination = 1
+                            btnCreatePolygon.visibility = View.VISIBLE
+                            drawPolygon()
+                            isMarker = true
+                            showAlertDialog()
+//                        showBottomSheet()
+                        }
                     }
                 }
-            }
 
-            return@OnTouchListener isMapMoveable
-        })
+                return@OnTouchListener isMapMoveable
+            })
+        }
     }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun showAlertDialog() {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Выберите объект:").setSingleChoiceItems(items, -1) { _, which ->
+            item = which
+        }.setPositiveButton("Ok") { _, _ -> }
+        builder.create().show()
+
+        if (isMarker) {
+            frame_layout.setOnTouchListener(View.OnTouchListener { _, motionEvent ->
+                if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+                    val point = Point(motionEvent.x.roundToInt(), motionEvent.y.roundToInt())
+                    val latLng = googleMap.projection.fromScreenLocation(point)
+                    val latitude = latLng.latitude
+                    val longitude = latLng.longitude
+                    isMarker = false
+                    if(PolyUtil.containsLocation(latLng, positions, true))
+                        setMarker(LatLng(latitude, longitude))
+                }
+
+                return@OnTouchListener isMapMoveable
+            })
+        }
+    }
+
+    private fun setMarker(latLng: LatLng) {
+        googleMap.addMarker(
+            MarkerOptions().position(latLng)
+                .icon(
+                    BitmapDescriptorFactory.defaultMarker(
+                        when (item) {
+                            0 -> BitmapDescriptorFactory.HUE_RED
+                            1 -> BitmapDescriptorFactory.HUE_BLUE
+                            2 -> BitmapDescriptorFactory.HUE_YELLOW
+                            else -> BitmapDescriptorFactory.HUE_GREEN
+                        }
+                    )
+                )
+        )
+    }
+
+//    private fun showBottomSheet() {
+//        CustomBottomSheetDialogFragment().apply {
+//            if (isAdded)
+//                show(childFragmentManager, CustomBottomSheetDialogFragment.TAG)
+//        }
+//    }
 
     private fun drawPolygon() {
         val polygonOptions = PolygonOptions()
