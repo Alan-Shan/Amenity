48e6cd0047d18233761509e66c77229cf0a0a955
Anastasia <ad4019809@gmail.com>
2020-12-12 21:01:05 +0300

    some ui changes

--- app/src/main/java/top/ilum/amenity/MainActivity.kt
+++ app/src/main/java/top/ilum/amenity/MainActivity.kt
@@ -2,11 +2,9 @@ package top.ilum.amenity
 
 import SharedPrefs
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -16,12 +14,7 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import top.ilum.amenity.data.User
-import top.ilum.amenity.utils.Builder
-import top.ilum.amenity.utils.Endpoints
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -52,6 +45,47 @@ class MainActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        btnCreatePolygon.text = resources.getText(R.string.txt_choose_fragment)
+        btnCreatePolygon.setOnClickListener {
+//            val fragment: HomeFragment = supportFragmentManager.findFragmentByTag("Home") as HomeFragment
+//            isMapMoveable = true
+//            btnCreatePolygon.visibility = View.GONE
+//            positions.removeAll(positions)
+//            googleMap.clear()
+//
+//            frame_layout.setOnTouchListener(View.OnTouchListener { _, motionEvent ->    //Draw polygon
+//                if (isMapMoveable) {
+//                    val point = Point(motionEvent.x.roundToInt(), motionEvent.y.roundToInt())
+//                    val latLng = googleMap.projection.fromScreenLocation(point)
+//                    val latitude = latLng.latitude
+//                    val longitude = latLng.longitude
+//
+//                    when (motionEvent.action) {
+//                        MotionEvent.ACTION_DOWN -> {
+//                            positions.add(LatLng(latitude, longitude))
+//                        }
+//                        MotionEvent.ACTION_MOVE -> {
+//                            positions.add(LatLng(latitude, longitude))
+//                        }
+//                        MotionEvent.ACTION_UP -> {
+//                            isMapMoveable = false
+//                            source = 0
+//                            destination = 1
+//                            btnCreatePolygon.visibility = View.VISIBLE
+//                            drawPolygon()
+//                            isMarker = true
+//                            showBottomSheet()
+//                        }
+//                    }
+//                }
+//
+//                return@OnTouchListener isMapMoveable
+//            })
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onSupportNavigateUp(): Boolean {
